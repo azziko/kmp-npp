e2510568d978242cb35f3e9992de24577179308c
Aziz <sharipov.wdev@gmail.com>
2025-05-17 19:35:00 +0200

    Add search within a single line

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 import Data.Array
 import Data.List
+import System.IO
+import Control.Monad
+import System.Environment 
 
 buildTable :: String -> Array Int Int
+buildTable [] = array (0, 0) []
 buildTable needle = listArray (0, n - 1) table
     where
         n = length needle
@@ -13,3 +17,28 @@ buildTable needle = listArray (0, n - 1) table
                 (j + 1) : build (i + 1) (j + 1)
             | j > 0 = build i (table !! (j - 1))
             | otherwise = 0 : build (i + 1) 0
+
+-- runKMP :: String -> [String] -> Bool
+-- runKMP needle lines = True
+
+searchInLine :: String -> String -> [Int]
+searchInLine needle haystack = search 0 0 []
+    where
+        n = length needle
+        m = length haystack
+        table = buildTable needle
+
+        search i j acc
+            | i >= m = reverse acc
+            | haystack !! i == needle !! j =
+                if j == n - 1
+                    then search (i + 1) (table ! j) ((i - j) : acc)
+                    else search (i + 1) (j + 1) acc
+            | j > 0 = search i (table ! (j - 1)) acc
+            | otherwise = search (i + 1) 0 acc
+
+main = do
+    (needle : filename : args) <- getArgs
+
+    contents <- readFile filename
+    print (searchInLine needle (head (lines contents)))
